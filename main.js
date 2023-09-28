aa3b6efcb3a51b41e4a1f7e0d44aff7bd6ae4d1c
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2023-09-27 18:17:46 -0700

    Adding the ability to test the template and test the scrollability of the website.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-function Test(){
-    print("hello")
-}
+function showContent() {
+    for(let i = 0; i < 100; i++) {
+        let temp = document.getElementsByTagName("template")[0];
+        let clon = temp.content.cloneNode(true);
+        document.body.appendChild(clon);
+    }
+    
+  }
