16c5cfb85ac0442cb8c1e22002a341a6b790410d
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2023-09-27 23:07:36 -0500

    Alot of changes to formatting

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function showContent() {
+function addItem() {
+    
+    let temp = document.getElementsByTagName("template")[0];
     for(let i = 0; i < 100; i++) {
-        let temp = document.getElementsByTagName("template")[0];
         let clon = temp.content.cloneNode(true);
         document.body.appendChild(clon);
     }
-    
-  }
+}
+window.onload = addItem;
