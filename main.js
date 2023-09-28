44a880c37719ade5a9e69013757dfe181f8f9c89
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2023-09-28 10:30:16 -0500

    fixing more syntax issues

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function addItem() {
     let temp = document.getElementsByTagName("template")[0];
     for(let i = 0; i < 100; i++) {
         let itemprice_itemstock = document.createTextNode("Price: "+i+" | Stock: "+i);
-        temp.append(itemprice_itemstock);
+        temp.body.append(itemprice_itemstock);
         let clon = temp.content.cloneNode(true);
         document.body.appendChild(clon);
     }
