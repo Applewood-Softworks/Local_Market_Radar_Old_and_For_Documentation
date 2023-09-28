3347a3c941492fa4c6a3c8445cf36318fb3128b5
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2023-09-28 10:28:38 -0500

    Adding the template for changing the price and stock of each item.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function addItem() {
     
     let temp = document.getElementsByTagName("template")[0];
     for(let i = 0; i < 100; i++) {
+        let itemprice_itemstock = document.createTextNode("Price: "+i+" | Stock: "+i)
+        temp.append("")
         let clon = temp.content.cloneNode(true);
         document.body.appendChild(clon);
     }
