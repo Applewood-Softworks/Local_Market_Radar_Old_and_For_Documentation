1ab31f02d0b915e7a9089272288b0ecfb4973f71
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2023-09-28 18:21:13 -0700

    syntax

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function addItem() {
     //let name = document.getElementsByTagName("template")[0];
-    let price_and_stock = document.getElementsByTagName("template")[1];
+    let price_and_stock = document.getElementsByIdName("template")[0];
     //let description = document.getElementsByTagName("template")[2];
     for(let i = 0; i < 100; i++) {
         let clon = price_and_stock.content.cloneNode(true);
-        let itemprice_itemstock = clon.querySelector("p");
+        let itemprice_itemstock = clon.querySelector("price");
         itemprice_itemstock.textContent = "Price: " + i + " | Stock: " + i; 
         
         document.body.appendChild(clon);
