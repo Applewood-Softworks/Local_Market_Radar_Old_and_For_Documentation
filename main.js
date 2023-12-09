aba9b3ed056e46bf0b2a24ce5c52da5f371d17a6
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2023-12-09 06:16:59 -0600

    Fixing formatting
    Fixed the footer location.

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 function addItem() {
-    //let name = document.getElementsByTagName("template")[0];
-    let price_and_stock = document.getElementsByTagName("template")[0];
-    let footer_template = document.getElementsByClassName("footer");
-    //let description = document.getElementsByTagName("template")[2];
-    for(let i = 0; i < 100; i++) {
+    let price_and_stock = document.getElementById("itemTemplate");
+    let footer_template = document.getElementById("footerTemplate");
+    let footer = document.getElementById("footer");
+    let main = document.getElementById("main");
+    for (let i = 0; i < 100; i++) {
         let clon = price_and_stock.content.cloneNode(true);
         let itemprice_itemstock = clon.querySelector("p");
-        itemprice_itemstock.textContent = "Price: " + i + " | Stock: " + i; 
-        
-        document.body.appendChild(clon);
-    }
-    let footer = footer_template.content.cloneNode(true);
-    document.body.appendChild(footer);
+        itemprice_itemstock.textContent = "Price: " + i + " | Stock: " + i;
 
+        main.appendChild(clon);
+        
+    }
+    let footer_content = footer_template.content.cloneNode(true);
+    footer.appendChild(footer_content);
 }
-window.onload = addItem();
+
+window.onload = addItem;
